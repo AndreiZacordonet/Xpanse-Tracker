981267af0d5bcd6cbee3e01a1e3d46b3252e5b95
Andrei Zacordoneț <andrei.zacordonet@amplified.software>
2026-05-25 17:09:35 +0300

    Feat: add iam role

--- credentials.py
+++ credentials.py
@@ -6,6 +6,7 @@ load_dotenv()
 AWS_ACCESS_KEY = os.getenv("aws_access_key_id")
 AWS_SECRET_KEY = os.getenv("aws_secret_access_key")
 AWS_SESSION_TOKEN = os.getenv("aws_session_token")
+ROLE_ARN = os.getenv("ROLE_ARN")
 
 REGION = "us-east-1" 
 
